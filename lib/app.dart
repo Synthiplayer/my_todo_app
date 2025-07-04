991e136d479e8a998c043f6644e7ba4b9d1f0be1
Stefan Rose <info@synthisoft.de>
2025-07-04 21:18:40 +0200

    problem gelöst icons funktioeren

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_todo_app/ui/theme.dart';
+import 'package:hollyday_todos/ui/theme.dart';
 import 'package:provider/provider.dart';
 import 'providers/todo_provider.dart';
 import 'screens/todo_list_screen.dart';
